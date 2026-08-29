0d01da680633fb9d78601480097b2ed540eb6cce
Alrae Joseph Ordonez <ordonezalraejoseph85@gmail.com>
2026-08-29 10:38:25 +0800

    add required fonts and images

--- itp107_assessment/lib/main.dart
+++ itp107_assessment/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,16 +13,23 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-            child: Column(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-                Text('Hello World!', style: TextStyle( fontFamily: 'Roboto', fontSize: 50), textAlign: TextAlign.center),
-                Image.asset('assets/images/booksample.webp', width: 120, height: 120,), 
-              ],
+              Text(
+                'Hello World!',
+                style: TextStyle(fontFamily: 'Roboto', fontSize: 50),
+                textAlign: TextAlign.center,
+              ),
+              Image.asset(
+                'assets/images/booksample.webp',
+                width: 120,
+                height: 120,
+              ),
+            ],
           ),
-        ) 
-       
+        ),
       ),
       debugShowCheckedModeBanner: false,
     );
